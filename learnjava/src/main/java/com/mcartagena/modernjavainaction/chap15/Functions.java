16ada4b283591de156ff444b836d408fcd140622
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2021-05-07 15:29:01 -0400

    Working with Threads, Futures, CallBacks and CompletableFutures.

---
+++ learnjava/src/main/java/com/mcartagena/modernjavainaction/chap15/Functions.java
@@ -0,0 +1,21 @@
+package com.mcartagena.modernjavainaction.chap15;
+
+public class Functions {
+    public static int f(int x) {
+        System.out.println("f(x) = " + x);
+        return x * 2;
+    }
+
+    public static int g(int x) {
+        System.out.println("g(x) = " + x);
+        return x + 1;
+    }
+
+    public static Integer fo(int x) {
+        return x * 2;
+    }
+
+    public static Integer go(int x) {
+        return x + 1;
+    }
+}
