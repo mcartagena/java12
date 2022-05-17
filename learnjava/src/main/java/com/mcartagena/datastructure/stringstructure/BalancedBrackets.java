5b8c80c5ae7aba8bf5104501de2126593e2af97a
mcartagena <marcelocartagenaorellana@gmail.com>
2022-05-16 21:21:06 -0400

    Working with stack and tries

--- learnjava/src/main/java/com/mcartagena/datastructure/stringstructure/BalancedBrackets.java
+++ learnjava/src/main/java/com/mcartagena/datastructure/stringstructure/BalancedBrackets.java
@@ -1,2 +1,76 @@
-package com.mcartagena.datastructure.stringstructure;public class BalancedBrackets {
+package com.mcartagena.datastructure.stringstructure;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+        // Write your code here
+        String result = "YES";
+        Map<Character, Character> mapCloseBracket = new HashMap<>();
+        mapCloseBracket.put('{','}');
+        mapCloseBracket.put('[',']');
+        mapCloseBracket.put('(',')');
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i< s.length(); i++){
+            Character value =  s.charAt(i);
+            if(value == '{' ||  value == '[' || value == '('){
+                stack.push(value);
+            } else {
+                if(stack.isEmpty()){
+                    result = "NO";
+                    break;
+                }
+                Character valueStack = stack.pop();
+
+                if(mapCloseBracket.get(valueStack) != value){
+                    result = "NO";
+                    break;
+                }
+            }
+        }
+        if(!stack.isEmpty()){
+            result = "NO";
+        }
+
+        return result;
+    }
+
 }
+
+public class BalancedBrackets {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
--- learnjava/src/main/java/com/mcartagena/datastructure/trie/Contacts.java
+++ learnjava/src/main/java/com/mcartagena/datastructure/trie/Contacts.java
@@ -1,2 +1,77 @@
-package com.mcartagena.datastructure.trie;public class Contacts {
+package com.mcartagena.datastructure.trie;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Contacts {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Trie trie = new Trie();
+
+        for (int i = 0; i < n; i++) {
+            String operation = scan.next();
+            String contact = scan.next();
+
+            if (operation.equals("add")){
+                trie.add(contact);
+
+            } else if (operation.equals("find")){
+                System.out.println(trie.find(contact));
+            }
+        }
+
+        scan.close();
+
+    }
+
+    public static class TrieNode {
+        private Map<Character, TrieNode> children = new HashMap<>();
+        private int size = 0;
+
+        public void addChildIfAbsent(char c) {
+            children.putIfAbsent(c, new TrieNode());
+        }
+
+        public TrieNode getChild(char c) {
+            return children.get(c);
+        }
+
+        public void addSize(int value) {
+            this.size += value;
+        }
+
+        public int getSize() {
+            return size;
+        }
+    }
+
+    public static class Trie {
+        private TrieNode root = new TrieNode();
+
+        public void add(String word) {
+            TrieNode curr = root;
+
+            for (Character c : word.toCharArray()) {
+                curr.addChildIfAbsent(c);
+                curr = curr.getChild(c);
+                curr.addSize(1);
+            }
+        }
+
+        public int find(String prefix) {
+            TrieNode curr = root;
+
+            for(Character c: prefix.toCharArray()) {
+                curr = curr.getChild(c);
+                if(curr == null){
+                    return 0;
+                }
+            }
+            return curr.getSize();
+        }
+    }
+
 }
